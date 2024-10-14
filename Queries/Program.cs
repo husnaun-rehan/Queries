fa2f85912dc391a55540ff27453191fb71d57f30
Hp <Hp@RANA-AMEER-HAMZ>
2024-10-14 19:50:41 +0500

    Add project files.

---
+++ Queries/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+namespace Queries
+{
+    class Program
+    {
+        private static object course;
+
+        static void Main(string[] args)
+        {
+            var context = new PlutoContext();
+            Course course1 = new Course();
+
+            //LINQ query
+
+            var query = 
+                from c in context.Courses
+                where c.Name.Contains("c#")
+                orderby c.Name
+                select c;
+
+            //foreach (var course in query) 
+
+            //    Console.WriteLine(course.Name);
+
+            //Extention Methods
+
+            var courses = context.Courses.Where(c => c.Name.Contains("c#")).OrderBy(c => c.Name);
+
+            foreach(var course in courses)
+                Console.WriteLine(course.Name);
+
+           
+
+
+        }
+    }
+}
